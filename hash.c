cbc64bfc5d99a6222320ab726c630935f40584c1
harshanavkis <harshanavkis@gmail.com>
2018-09-28 22:43:03 +0530

    Child creation and deletion

--- hash.c
+++ hash.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include<sys/wait.h>
 #include<string.h>
 
 int main(int argc, char* argv[])
@@ -14,16 +15,34 @@ int main(int argc, char* argv[])
   
   char* exitCmd = "exit\n";
   char *cmd = NULL;
-  size_t bufLen = 0;
+  size_t bufLen = 0;  
 
+  printf(">hash ");
   while(1)
     {
-      printf(">hash ");
-
       getline(&cmd, &bufLen, stdin);
-            
+      
       if(strncmp(exitCmd, cmd, 5)==0)
 	exit(0);
-      printf("%s", cmd);      
+
+      int rc = fork();
+
+      if(rc<0)
+	{
+	  fprintf(stderr, "Unable to create child process\n");
+	  exit(1);
+	}
+      else if(rc==0)
+	{
+	  printf("In child\n");
+	  printf("%s", cmd);
+	  break;
+	}
+      else
+	{
+	  wait(NULL);
+	  printf("In parent\n");
+	  printf(">hash ");
+	}
     }
 }
